9187fc152ad00382cb86fb3b8d7a2abca4a66ae7
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-09-28 13:37:02 -0500

    Further Updates

---
+++ py_client/basic.py
@@ -0,0 +1,13 @@
+import requests
+
+
+endpoint = 'https://httpbin.org/status/200'
+endpoint = 'https://httpbin.org/anything'
+
+
+get_response = requests.get(endpoint, json={'query': 'Hello World'})
+
+print(get_response.json())
+
+
+print(get_response.text)
